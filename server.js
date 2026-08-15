cd805df64932bf44f18686fac65f612df82de57a
Harshi <tandelharshi3718@gmail.com>
2026-08-15 21:33:52 +0530

    Fix product model filename

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/Product");
+const Product = require("./models/product");
 const Transaction = require("./models/Transaction");
 const cors = require("cors");
 const app = express();
